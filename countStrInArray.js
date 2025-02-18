8e73ffadd6f28e7b79526bd4ceba57b592c616c0
paklong <paklong2556@gmail.com>
2025-02-17 22:04:45 -0800

    Add countStrInArray

---
+++ countStrInArray.js
@@ -0,0 +1,19 @@
+function countStr(arr) {
+  if (arr.length == 0) return 0;
+  return arr[0].length + countStr(arr.slice(1));
+}
+
+
+function test() {
+  const test1 = ['ab', 'c', 'def', 'ghij'];
+  console.log(`Expect ${countStr(test1)} to be 10`);
+  const test2 = ['ab', 'c', 'def', 'ghij', 'k'];
+  console.log(`Expect ${countStr(test2)} to be 11`);
+  const test3 = [];
+  console.log(`Expect ${countStr(test3)} to be 0`);
+  const test4 = ['abc'];
+  console.log(`Expect ${countStr(test4)} to be 3`);
+}
+
+test();
+
