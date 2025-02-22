cee292f376144686f321ce0985ac32c4124c341c
paklong <paklong2556@gmail.com>
2025-02-22 12:24:09 -0800

    Complete exercise 3

--- react/renderingList/exercise3/src/App.jsx
+++ react/renderingList/exercise3/src/App.jsx
@@ -1,21 +1,28 @@
 import './App.css'
 import { recipes } from './data/data'
 
+
+function Recipe({ id, name, ingredients }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <ul>'
+        {ingredients.map(ingredient =>
+          <li key={ingredient}>
+            {ingredient}
+          </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
 export default function App() {
   return (
     <div>
       <h1>Recipes</h1>
       {recipes.map(recipe =>
-        <div key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <ul>
-            {recipe.ingredients.map(ingredient =>
-              <li key={ingredient}>
-                {ingredient}
-              </li>
-            )}
-          </ul>
-        </div>
+        <Recipe key={recipe.id} {...recipe} />
       )}
     </div>
   );
