42da159d9df7ea78327ee73400476c4858f77d0f
paklong <paklong2556@gmail.com>
2025-02-22 11:12:20 -0800

    Add rendering list exercise 2 solution

--- react/renderingList/exercise2/src/App.jsx
+++ react/renderingList/exercise2/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment } from 'react';
+import { recipes } from './data/data';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function IngredientList({ ingredients }) {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    ingredients.map(ingredient => {
+      return <li key={ingredient}>{ingredient}</li>
+    }
+    )
+  );
 }
 
-export default App
+function RecipeList({ recipes }) {
+  return (
+    recipes.map(recipe =>
+      <Fragment key={recipe.id}>
+        <h2>{recipe.name}</h2>
+        <ul>
+          <IngredientList ingredients={recipe.ingredients} />
+        </ul>
+      </Fragment>
+    )
+
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <h1>Recipes</h1>
+      <RecipeList
+        recipes={recipes}
+      />
+    </div>
+  );
+}
