887061db4bde6c61c4448f238ee01f9461a24390
paklong <paklong2556@gmail.com>
2025-02-22 11:15:58 -0800

    Set up exercise 3 folder

---
+++ react/renderingList/exercise3/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { recipes } from './data/data'
+
+export default function App() {
+  return (
+    <div>
+      <h1>Recipes</h1>
+      {recipes.map(recipe =>
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+            {recipe.ingredients.map(ingredient =>
+              <li key={ingredient}>
+                {ingredient}
+              </li>
+            )}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
