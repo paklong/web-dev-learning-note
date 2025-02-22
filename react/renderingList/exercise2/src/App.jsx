eb06680c85158372fb510d817dfd4a439e1f4849
paklong <paklong2556@gmail.com>
2025-02-22 11:33:49 -0800

    Moved <li> map inside <Fragment> map

--- react/renderingList/exercise2/src/App.jsx
+++ react/renderingList/exercise2/src/App.jsx
@@ -1,22 +1,17 @@
 import { Fragment } from 'react';
 import { recipes } from './data/data';
 
-function IngredientList({ ingredients }) {
-  return (
-    ingredients.map(ingredient => {
-      return <li key={ingredient}>{ingredient}</li>
-    }
-    )
-  );
-}
-
 function RecipeList({ recipes }) {
   return (
     recipes.map(recipe =>
       <Fragment key={recipe.id}>
         <h2>{recipe.name}</h2>
         <ul>
-          <IngredientList ingredients={recipe.ingredients} />
+          {recipe.ingredients.map(ingredient =>
+            <li key={ingredient}>
+              {ingredient}
+            </li>
+          )}
         </ul>
       </Fragment>
     )
