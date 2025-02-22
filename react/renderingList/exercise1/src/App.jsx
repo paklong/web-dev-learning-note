0afc03e01bad00659cdf91b98a589a6fd888c7d2
paklong <paklong2556@gmail.com>
2025-02-22 08:56:08 -0800

    Add exercise 1 solution, feeling good

--- react/renderingList/exercise1/src/App.jsx
+++ react/renderingList/exercise1/src/App.jsx
@@ -2,24 +2,41 @@ import './App.css'
 import { people } from './data/data.js'
 import { getImageUrl } from './utils/utils.js'
 
-function App() {
-  const listItems = people.map(person =>
-    <li key={person.id}>
-      <img
-        src={getImageUrl(person)}
-        alt={person.name}
-      />
-      <p>
-        <b>{person.name}:</b>
-        {' ' + person.profession + ' '}
-        known for {person.accomplishment}
-      </p>
-    </li>
+function MyHeroList({ title, people }) {
+  return (
+    <>
+      <h1>{title.toUpperCase()}</h1>
+      {people.map(person =>
+        <li key={person.id}>
+          <img
+            src={getImageUrl(person)}
+            alt={person.name}
+          />
+          <p>
+            <b>{person.name}: </b>
+            {person.profession}
+            <br />
+            Known for {person.accomplishment}
+          </p>
+        </li>
+      )}
+    </>
   );
+}
+
+function App() {
+  const chemists = people.filter(person => person.profession == 'chemist');
+  const otherCoolScientists = people.filter(person => person.profession !== 'chemist');
   return (
     <article>
-      <h1>Scientists</h1>
-      <ul>{listItems}</ul>
+      <MyHeroList
+        title='Chemists'
+        people={chemists}
+      />
+      <MyHeroList
+        title='Other cool scientists'
+        people={otherCoolScientists}
+      />
     </article>
   );
 }
