20b1dc804a959c8e0acc7582ba49ec3c6973f9a7
paklong <paklong2556@gmail.com>
2025-02-22 22:04:50 -0800

    Add shortestPath.js

--- shortestPath.js
+++ shortestPath.js
@@ -1,6 +1,13 @@
 
+//  if (row === 1 || col === 1) return 1;
+//  return shortestPath(row - 1, col) + shortestPath(row, col - 1)
+//}
 
-
+function shortestPath(row, col) {
+  if (row === 1 || col === 1) return 1;
+  let total = shortestPath(row - 1, col) + shortestPath(row, col - 1)
+  return total
+}
 
 
 
@@ -14,7 +21,7 @@ function test() {
   console.log(`Expect ${shortestPath(row = 2, col = 2)} to be 2`);
   console.log(`Expect ${shortestPath(row = 2, col = 3)} to be 3`);
   console.log(`Expect ${shortestPath(row = 3, col = 2)} to be 3`);
-  console.log(`Expect ${shortestPath(row = 3, col = 3)} to be 5`);
+  console.log(`Expect ${shortestPath(row = 3, col = 3)} to be 6`);
 }
 
 test();
