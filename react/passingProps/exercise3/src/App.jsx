6c0d1b51dd033b464da8554be93590f673b4e50e
paklong <paklong2556@gmail.com>
2025-02-25 18:21:13 -0800

    Add passingProps exercise 3 solution

---
+++ react/passingProps/exercise3/src/App.jsx
@@ -0,0 +1,34 @@
+import './App.css'
+
+function Card({ title, children }) {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h1>{title}</h1>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+
+function App() {
+  return (
+    <div>
+      <Card title="Photo">
+        <img
+          className="avatar"
+          src="https://i.imgur.com/OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={70}
+          height={70}
+        />
+      </Card>
+      <Card title="About">
+        <p>Aklilu Lemma was a distinguished Ethiopian scientist who discovered a natural treatment to schistosomiasis.</p>
+      </Card>
+    </div>
+  );
+}
+
+export default App
