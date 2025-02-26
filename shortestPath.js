d34f16085a7350e9d8ec9c736b5cefc69980970d
paklong <paklong2556@gmail.com>
2025-02-25 18:40:02 -0800

    Add more solution to shortestPath.js

--- shortestPath.js
+++ shortestPath.js
@@ -9,8 +9,27 @@ function shortestPath(row, col) {
   return total
 }
 
+// This is a solution without recurion
+// Don't use Array(3).fill(Array(3).fill(0)); because array is passed by reference
+// This way will have saem reference of array accros row
+function shortestPath(row, col) {
+  let dp = Array(row).fill(0).map(() => Array(col).fill(0));
 
+  // Base case: First row and first column have only one way to be reached
+  for (let i = 0; i < row; i++) dp[i][0] = 1;
+  for (let j = 0; j < col; j++) dp[0][j] = 1;
 
+  // Fill the dp table using bottom-up approach
+  for (let i = 1; i < row; i++) {
+    for (let j = 1; j < col; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+    }
+  }
+
+  return dp[row - 1][col - 1];
+}
+
+console.log(shortestPath(3, 3)); // Output: 6
 
 
 
