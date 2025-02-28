0686f6334e41293ed90ff4aa74ceec772abe0f4c
paklong <paklong2556@gmail.com>
2025-02-27 20:50:33 -0800

    Set up state-a-components-memory
    Set up for state-a-components-memory exercise again

---
+++ react/state-a-components-memory/exercise4/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  const [name, setName] = useState('');
+
+  function handleClick() {
+    setName(prompt('What is your name?'));
+    alert(`Hello, ${name}!`);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Greet
+    </button>
+  );
+}
