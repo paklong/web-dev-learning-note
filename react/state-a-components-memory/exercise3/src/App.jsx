7a0a662f5d1f1565a5da4a97860b44c0b88fa95b
paklong <paklong2556@gmail.com>
2025-02-28 20:11:36 -0800

    Complete exercise 3

--- react/state-a-components-memory/exercise3/src/App.jsx
+++ react/state-a-components-memory/exercise3/src/App.jsx
@@ -3,25 +3,23 @@ import { useState } from 'react';
 
 export default function App() {
   const [isSent, setIsSent] = useState(false);
+  const [message, setMessage] = useState('');
   if (isSent) {
     return <h1>Thank you!</h1>;
-  } else {
-    // eslint-disable-next-line
-    const [message, setMessage] = useState('');
-    return (
-      <form onSubmit={e => {
-        e.preventDefault();
-        alert(`Sending: "${message}"`);
-        setIsSent(true);
-      }}>
-        <textarea
-          placeholder="Message"
-          value={message}
-          onChange={e => setMessage(e.target.value)}
-        />
-        <br />
-        <button type="submit">Send</button>
-      </form>
-    );
   }
+  return (
+    <form onSubmit={e => {
+      e.preventDefault();
+      alert(`Sending: "${message}"`);
+      setIsSent(true);
+    }}>
+      <textarea
+        placeholder="Message"
+        value={message}
+        onChange={e => setMessage(e.target.value)}
+      />
+      <br />
+      <button type="submit">Send</button>
+    </form>
+  );
 }
